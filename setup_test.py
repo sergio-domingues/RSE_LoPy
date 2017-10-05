cb2824fae2332f2a596dda2766fcfca3b589bbb0
sergio <sergio.mieic@gmail.com>
2017-10-05 17:20:41 +0200

    libs

---
+++ setup_test.py
@@ -0,0 +1,20 @@
+import pycom
+import time
+
+RED = 0xFF0000
+YELLOW = 0xFFFF33
+GREEN = 0x007F00
+OFF = 0x000000
+
+def set_led_to(color=GREEN):
+    pycom.heartbeat(False) # Disable the heartbeat LED
+    pycom.rgbled(color)
+
+def flash_led_to(color=GREEN, t1=1):
+    set_led_to(color)
+    time.sleep(t1)
+    set_led_to(OFF)
+
+flash_led_to(RED)
+flash_led_to(YELLOW)
+set_led_to(OFF)
